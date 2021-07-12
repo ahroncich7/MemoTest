81258a2fdc838df48b10c2f003474e138cad4483
ahroncich7 <alehroncich@gmail.com>
2021-07-12 13:25:27 -0300

    Define las funciones para crear el mazo de tarjetas aletarorias
    Tambien cambia nombres de variables y reorganiza

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-let cards = document.querySelectorAll("img")
-let cardsEnJuego = []
-
-cards.forEach(function(imageElement) {
-    imageElement.onclick = function(e) {
-        card = e.target
-        cardsEnJuego.push(card)
-        if (cardsEnJuego.length >= 2) {
-            if (cardsEnJuego[0].src === cardsEnJuego[1].src) {
-                cardsEnJuego[0].src = "images/cards/none.jpg"
-                cardsEnJuego[1].src = "images/cards/none.jpg"
-            }
-            cardsEnJuego = []
-            return false
+let listaFinal = [];
+let tarjetas = document.querySelectorAll(".tarjetas")
+let tarjetasEnJuego = []
+let colores = ["negro", "azul", "amarillo", "rojo", "verde", "violeta", "naranja", "gris", "negro", "azul", "amarillo", "rojo", "verde", "violeta", "naranja", "gris"];
+function obtenerLista() {
+    let numeroUnico
+    while (listaFinal.length < 16) {
+        numeroUnico = Math.floor(Math.random() * 16)
+        if (comprobarSiEsUnico(numeroUnico)) {
+            listaFinal.push(numeroUnico)
         }
     }
-});
+    return listaFinal
+}
+
+function comprobarSiEsUnico(n) {
+    let esUnico = true
+    listaFinal.forEach(function(i) {
+        if (i === n) {
+            esUnico = false
+        }
+    });
+    return esUnico
+}
+
+function asignarTarjetas() {
+    let j = 0
+    listaFinal.forEach(function(n) {
+        tarjetas[n].querySelector("img").src = `images/cards/${colores[j]}.jpg`
+        j++
+    });
+}
+
