8e52f4ee33dfa0fc1fe02affd7483b26673a17cb
ahroncich7 <alehroncich@gmail.com>
2021-07-12 13:38:51 -0300

    Reemplaza tarjetas por $tarjetas
    Lo hace porque a $tarjetas se le asignan los elementos div que adentro tienen las tarjetas, como un NodeList

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let listaFinal = [];
-let tarjetas = document.querySelectorAll(".tarjetas")
+let $tarjetas = document.querySelectorAll(".tarjetas")
 let tarjetasEnJuego = []
 let colores = ["negro", "azul", "amarillo", "rojo", "verde", "violeta", "naranja", "gris", "negro", "azul", "amarillo", "rojo", "verde", "violeta", "naranja", "gris"];
 iniciarJuego();
+
+
 function obtenerLista() {
     let numeroUnico
     while (listaFinal.length < 16) {
@@ -27,7 +29,7 @@ function comprobarSiEsUnico(n) {
 function asignarTarjetas() {
     let j = 0
     listaFinal.forEach(function(n) {
-        tarjetas[n].querySelector("img").src = `images/cards/${colores[j]}.jpg`
+        $tarjetas[n].querySelector("img").src = `images/cards/${colores[j]}.jpg`
         j++
     });
 }
